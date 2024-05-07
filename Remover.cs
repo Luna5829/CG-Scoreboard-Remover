61ffc2d93449d1d6518d4bb7016873b9833be1c5
Luna <102811601+Luna5829@users.noreply.github.com>
2024-05-07 10:51:14 +0200

    Add files via upload

---
+++ Remover.cs
@@ -0,0 +1,29 @@
+﻿using BepInEx;
+
+using UnityEngine;
+
+namespace CGSR
+{
+    [BepInPlugin("CGSR", "CGSR", "1.0.0")]
+    public class Remover : BaseUnityPlugin
+    {
+        void Update()
+        {
+            if (SceneHelper.CurrentScene == "Endless")
+            {
+                GameObject Panel = GameObject.Find("Everything/Cube/Canvas");
+
+                if (Panel == null)
+                {
+                    return;
+                }
+
+                if (Panel.activeSelf == true)
+                {
+                    Panel.gameObject.SetActive(false);
+                    Logger.LogInfo("Disabled Scoreboard.");
+                }
+            }
+        }
+    }
+}
